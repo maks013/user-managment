aa705ddcd5d980727ae3ec18297dcea064267e43
Maksymilian Ulanecki <maks.u@vp.pl>
2023-11-08 20:49:23 +0100

    added jwt configuration properties

--- src/main/java/com/usermanagment/UserManagmentApplication.java
+++ src/main/java/com/usermanagment/UserManagmentApplication.java
@@ -1,8 +1,11 @@
 package com.usermanagment;
 
+import com.usermanagment.infrastructure.jwt.configuration.properties.JwtConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(JwtConfigurationProperties.class)
 @SpringBootApplication
 public class UserManagmentApplication {
     public static void main(String[] args) {
