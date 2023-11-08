d046e8cd692acf012ebedc899d08b7d5f1fee230
Maksymilian Ulanecki <maks.u@vp.pl>
2023-11-08 20:35:33 +0100

    simplified UserDataValidator

--- src/main/java/com/usermanagment/user/domain/UserDataValidator.java
+++ src/main/java/com/usermanagment/user/domain/UserDataValidator.java
@@ -3,20 +3,17 @@ package com.usermanagment.user.domain;
 import com.usermanagment.user.exception.InvalidEmailFormatException;
 import com.usermanagment.user.exception.TakenEmailException;
 import com.usermanagment.user.exception.TakenUsernameException;
+import lombok.RequiredArgsConstructor;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@RequiredArgsConstructor
 class UserDataValidator implements UserValidationService {
 
     private final UserRepository userRepository;
 
-    UserDataValidator(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    private static final String EMAIL_PATTERN =
-            "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$";
+    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$";
 
     @Override
     public void isUsernameAvailable(String username) {
