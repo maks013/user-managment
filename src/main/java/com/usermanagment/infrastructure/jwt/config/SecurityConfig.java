f390252d5d679823f2479f60340dc5d498462dfa
Maksymilian Ulanecki <maks.u@vp.pl>
2023-11-08 20:51:04 +0100

    updated security configuration

---
+++ src/main/java/com/usermanagment/infrastructure/jwt/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package com.usermanagment.infrastructure.jwt.config;
+
+import com.usermanagment.infrastructure.jwt.JwtAuthTokenFilter;
+import com.usermanagment.infrastructure.jwt.LoginUserDetailsService;
+import com.usermanagment.user.domain.UserFacade;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.time.Clock;
+
+@Configuration
+@AllArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthTokenFilter jwtAuthTokenFilter;
+
+    @Bean
+    Clock clock() {
+        return Clock.systemUTC();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserFacade userFacade) {
+        return new LoginUserDetailsService(userFacade);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable();
+        httpSecurity.authorizeRequests()
+                .antMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .headers().frameOptions().disable()
+                .and().httpBasic().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .exceptionHandling()
+                .and()
+                .addFilterBefore(jwtAuthTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
+
+}
